522a0f19c7af90ae2c4520a196dc0a22b554362d
= <dvlpasapro@gmail.com>
2025-11-30 14:51:01 +0900

    learn-how-to-use-before-in-rspec

--- spec/sample_spec.rb
+++ spec/sample_spec.rb
@@ -10,20 +10,20 @@ end
 
 RSpec.describe User do
   describe "#greet" do
-  context '12歳以下の場合'
-    it 'ひらがなで答えること' do
-      user = User.new
-      user.name = 'たろう'
-      user.age  = 12
-      expect(user.greet).to eq 'ぼくはたろうだよ。'
+    before do
+      @params = {name: 'たろう'}
     end
-  end
+    context '12歳以下の場合' do
+      it 'ひらがなで答えること' do
+        user = User.new(**@params.merge(age: 12))
+        expect(user.greet).to eq 'ぼくはたろうだよ。'
+      end
+    end
     context '13歳以上の場合' do
-    it '漢字で答えること' do
-      user = User.new
-      user.name = 'たろう'
-      user.age  = 13
-      expect(user.greet).to eq '僕はたろうです。'
+      it '漢字で答えること' do
+        user = User.new(**@params.merge(age: 13))
+        expect(user.greet).to eq '僕はたろうです。'
+      end
     end
   end
 end
